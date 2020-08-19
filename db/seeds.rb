dd4a1274480ceb63186a7482d23df8224e09ddd6
Tommy Wentling <tommy.wentling@gmail.com>
2020-08-19 16:15:57 +0200

    Add RestaurantsController, ReviewsController | Add Restaurants Views, Reviews Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+
+pits = Restaurant.create(name: "Le Pits", address: "123 AMS Way", category:"italian")
+leb = Restaurant.create(name: "Lebanese Saraja", address: "876 Canal Way", category:"japanese")
+tao = Restaurant.create(name: "Tao", address: "Raday Utca", category:"chinese")
+trat_tosc = Restaurant.create(name: "Tratoria Toscana", address: "BP", category:"italian")
+eiffel = Restaurant.create(name: "Eiffel 65", address: "Blue Way, Paris", category:"french")
+
+puts 'Finished!'
